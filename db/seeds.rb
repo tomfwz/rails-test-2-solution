9f78149f9822c929e062a2fa68526abc00f906dd
Steven Yap <stevenyap@futureworkz.com>
2015-11-03 15:32:15 +0700

    Add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+[
+  'Is Ruby better than Python?',
+  'Should abortion be legal?',
+  'Should gay marriage be legal?',
+  'Are fat people lazy?',
+  'Is SASS better than LESS?',
+  'Is ReactJS better than AngularJS?'
+].each do |content|
+  ap Topic.where(content: content).first_or_create!
+end
